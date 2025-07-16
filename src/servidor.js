6cddb5e50a27d29107b32ba8fa7324ac8cd46845
Alexandre-Passos <alexandre15passos@gmail.com>
2025-07-16 11:11:28 -0300

    Alura Docs

---
+++ src/servidor.js
@@ -0,0 +1,26 @@
+import express from "express";
+const app = express();
+const PORT = process.env.port || 3000;
+
+import url from "url";
+import path from "path";
+import http from "http";
+import { Server } from "socket.io";
+
+
+const caminhoAtual = url.fileURLToPath(import.meta.url);
+const diretorioPublico = path.join(caminhoAtual, "../..", "public");
+app.use(express.static(diretorioPublico));
+
+
+const servidorHttp = http.createServer(app);
+
+servidorHttp.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+})
+
+const io = new Server(servidorHttp);
+
+io.on("connection", () => { 
+    console.log("Um cliente se conectou.");
+})
