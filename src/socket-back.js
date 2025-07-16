6a1ef8b18a0a120ec622a330ada20284b207e264
Alexandre-Passos <alexandre15passos@gmail.com>
2025-07-16 13:13:09 -0300

    recebendo o valor escrito e enviando para o outro usuario

--- src/socket-back.js
+++ src/socket-back.js
@@ -3,6 +3,6 @@ import io from "./servidor.js";
 io.on("connection", (socket) => {
     console.log(`Um cliente com id ${socket.id} se conectou.`);
     socket.on("texto_editor", (texto) => {
-        console.log(texto);
+        socket.broadcast.emit("texto_editor_clientes", texto);
     })
 })
